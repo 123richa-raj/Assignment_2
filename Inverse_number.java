80db78b5f39b35131ff63fda57a3b21722c37c51
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-10 23:16:58 +0530

    Add files via upload

---
+++ Inverse_number.java
@@ -0,0 +1,24 @@
+package Assignment_2;
+
+import java.util.Scanner;
+
+public class Inverse_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int pos = 1;
+		int sum = 0;
+		while (n > 0) {
+			int rem = n % 10;
+			 sum += (sum + pos * Math.pow(10, rem - 1));
+
+
+			pos++;
+			n /= 10;// n = n/10;
+		}
+		System.out.println(sum);
+	}
+}
+
